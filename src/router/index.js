da135f7ed7ff47d9eb33735e169159178842e50b
Gerald <gpserrano.18.4@gmail.com>
2023-09-27 17:50:40 -0500

    feat: add personal data component and route

--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,12 @@
 
 
 import {createRouter, createWebHistory} from "vue-router";
+import personalDataComponent from "@/profile-management/components/personal-data-component.vue";
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-
-
+        {path: "/personal-data", component: personalDataComponent}
     ]
 })
 export default router;
