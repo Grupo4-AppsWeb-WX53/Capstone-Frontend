3caa7aa45947eb60ebbd2833305f8e82eace21c2
Mijael Yen Quispe <mijael.yq2002@outlook.com>
2023-09-26 23:43:01 -0500

    feat: install dependencies and prepare project

--- src/main.js
+++ src/main.js
@@ -1,6 +1,30 @@
 import './assets/main.css'
+import 'primevue/resources/themes/md-dark-indigo/theme.css';
+import "primevue/resources/primevue.min.css";
+import "primeicons/primeicons.css";
+import 'primeflex/primeflex.css';
+
+import Card from 'primevue/card';
+import Button from 'primevue/button';
+import SelectButton from 'primevue/selectbutton';
+import Sidebar from 'primevue/sidebar';
+import Avatar from 'primevue/avatar';
+import Menu from 'primevue/menu';
+import Menubar from 'primevue/menubar';
+import Toolbar from 'primevue/toolbar';
 
 import { createApp } from 'vue'
-import App from './App.vue'
+import App from './App.vue';
+import PrimeVue from "primevue/config";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(PrimeVue);
+app.component('pv-card', Card);
+app.component('pv-button', Button);
+app.component('pv-select-button', SelectButton);
+app.component('pv-sidebar', Sidebar);
+app.component('pv-avatar', Avatar);
+app.component('pv-menu', Menu);
+app.component('pv-menubar', Menubar);
+app.component('pv.toolbar', Toolbar);
+app.mount('#app');
